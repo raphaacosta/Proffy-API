75e3ba8739bcd3e6bd2f6487fa88b3a641cacfef
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-04 10:29:15 -0300

    changing database from sqlite to postgres

--- knexfile.ts
+++ knexfile.ts
@@ -1,44 +1,18 @@
 import path from 'path';
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
-    },
-    migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    },
-    useNullAsDefault: true,
+  client: 'postgres',
+  connection:{
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'rootroot',
+    database: 'proffy',
   },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: path.resolve(__dirname, 'src','database', 'migrations'),
   },
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  pool: {
+    min: 2,
+    max: 10
+  },
 };
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,15 +1,19 @@
 import knex from 'knex';
 import path from 'path';
 
-const db = knex({
-  client: 'sqlite3',
+const database = knex({
+  /*client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite')
   },
-  useNullAsDefault: true,
-  /*client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  searchPath: ['knex', 'public'],*/
+  useNullAsDefault: true,*/
+  client: 'postgres',
+  connection:{
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'rootroot',
+    database: 'proffy',
+  }
 });
 
-export default db;
+export default database;
