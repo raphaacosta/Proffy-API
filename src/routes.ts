8cc3ec169cce5fb140a33e0bb03c4395894cf170
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-11 21:41:13 -0300

    adding new routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,29 @@
 import express from 'express';
+
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import UsersController from './controllers/UsersConttroller';
+import AuthController from './controllers/AuthControler';
+import { auth } from './middlewares/auth';
 
 const routes = express.Router();
 const classesControllers = new ClassesController;
 const connectionsController = new ConnectionsController;
+const usersController = new UsersController;
+const authController = new AuthController;
 
-routes.post('/classes', classesControllers.create);
+routes.post('/users', usersController.create);
+
+routes.post('/login', authController.create);
+routes.put('/forgot_password', authController.forgotPassword);
+
+routes.use(auth);
+
+routes.post('/recover_password/:id', authController.recoverPassword);
+
 routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+routes.put('/classes/:userId', classesControllers.update);
 
 routes.post('/connections', connectionsController.create);
 routes.get('/connections', connectionsController.index);
