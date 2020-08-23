3ee4a01b58e64277dba6963b65040f675f3b4dba
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-23 15:02:43 -0300

    remove production repo

--- dist/src/utils/convertHourToMinutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function convertHourToMinute(time) {
-    const [hour, minutes] = time.split(':').map(Number);
-    const timeInMinutes = (hour * 60) + minutes;
-    return timeInMinutes;
-}
-exports.default = convertHourToMinute;
